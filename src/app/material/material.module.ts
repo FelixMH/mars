1119b2d71e90ed92f90c941fc846a73c9d9f0df2
FelixMH <felixmtzh.fm@gmail.com>
2018-09-20 01:31:51 -0500

    New changes to this file to main file too.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,23 +3,31 @@ import { CommonModule } from '@angular/common';
 import {
   MatToolbarModule,
   MatButtonModule,
-  MatIconModule
+  MatIconModule,
+  MatExpansionModule,
+  MatInputModule
 } from '@angular/material';
 
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
+import { MaterialButtonComponent } from './material-button/material-button.component';
+import { MaterialInputComponent } from './material-input/material-input.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatExpansionModule,
+    MatInputModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatExpansionModule,
+    MatInputModule
   ],
-  declarations: [MaterialDemoComponent]
+  declarations: [MaterialDemoComponent, MaterialButtonComponent, MaterialInputComponent]
 })
 export class MaterialModule { }
