817767f3df76d5f6804215a60849702b1844ee14
FelixMH <felixmtzh.fm@gmail.com>
2018-09-22 01:03:57 -0500

    Componente de lista => new

--- src/app/material/material-list/material-list.component.ts
+++ src/app/material/material-list/material-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Estudiante } from '../../shared/model/estudiante';
 
 @Component({
   selector: 'dev-material-list',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaterialListComponent implements OnInit {
 
+  estudiante1: Estudiante;
+  estudiante2: Estudiante;
+  estudiante3: Estudiante;
+  estudiante4: Estudiante;
+
   constructor() { }
 
   ngOnInit() {
+    console.log('Inicializando el componente Material List');
+    this.estudiante1 = {
+      id: 1,
+      nombre: 'Felix Martinez',
+      ciudad: 'Torreon,Coahuila'
+    };
+    this.estudiante2 = {
+      id: 2,
+      nombre: 'Luz María H. S',
+      ciudad: 'Torreon,Coahuila'
+    };
+    this.estudiante3 = {
+      id: 3,
+      nombre: 'Felix Martinez Rangel',
+      ciudad: 'Torreon,Coahuila'
+    };
+    this.estudiante4 = {
+      id: 4,
+      nombre: 'Barbara Martinez',
+      ciudad: 'Torreon,Coahuila'
+    };
   }
 
 }
