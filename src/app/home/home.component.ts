a46a2f6f73f580224720fd2ab8f1bb6a59cf275c
FelixMH <felixmtzh.fm@gmail.com>
2018-09-26 02:36:25 -0500

    Modificados archivos creados anteriormente y añadida la interface Apod.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Apod } from '../shared/model/apod';
 
 @Component({
   selector: 'dev-home',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  apod: Apod;
   constructor() { }
 
   ngOnInit() {
+    this.apod = {
+      date: '2018-07-20',
+      explanation: 'What causes Hubbles Variable',
+      hdurl: 'https://apod.nasa.gov/apod/image/1711/HubblesVariableNebula',
+      media_type: 'image',
+      service_version: 'v1',
+      title: 'What Causes Hubbles Variable',
+      url: 'https://apod.nasa.gov/apod/image/1711/HubblesVariableNebula'
+    };
   }
 
 }
